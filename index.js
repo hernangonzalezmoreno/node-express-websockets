48c1d6f479d60983ef8408b0d4172f78efe8e5e7
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2021-08-17 16:02:27 -0300

    Iniciando server y websocket

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+
+// settings
+app.set( 'port', process.env.PORT || 3000 );
+
+// static files
+app.use( express.static( path.join( __dirname, 'public' ) ) );
+
+// start the server
+const server = app.listen( app.get('port'), () => {
+    console.log( 'Server on port', app.get('port') );
+});
+
+// websockets
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+io.on('connection', () => {
+    console.log( 'new connection' );
+});
